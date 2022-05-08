87de318760b087c87813e5a17e06041d320ef155
Pierre Jarry <pierre.jarry82@gmail.com>
2022-05-08 12:03:10 +0200

    Added cookie banner title and toggle switch, css sizes adjustments

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-const infoDiv = document.querySelector('.main-content__info');
-const closeIcon = document.querySelector('.main-content__info .close-icon');
+const infoDiv = document.querySelector('.main-content__main__info');
+const closeIcon = document.querySelector('.main-content__main__info .close-icon');
 
 const closeDiv = () => infoDiv.remove();
 
