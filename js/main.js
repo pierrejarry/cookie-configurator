13cef4499d185d107c2d483d25cb1251be837cbf
Pierre Jarry <pierre.jarry82@gmail.com>
2022-05-08 15:56:39 +0200

    Added position and theme divs. Added js to select an item

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 const infoDiv = document.querySelector('.main-content__main__info');
 const closeIcon = document.querySelector('.main-content__main__info .close-icon');
 
+/* Close Information div when clicking on close icon */
 const closeDiv = () => infoDiv.remove();
+
+
+/* Add active class when clicking on an item list */
+
+document.addEventListener('click', function handleClick(event) {
+    const listItem = event.target,
+          listParent = listItem.closest('ul'),
+          allItems = listParent.querySelectorAll('.list-item');
+
+    for (item of allItems) {
+        item.classList.remove('active')
+    }
+
+    listItem.classList.contains('list-item') ? 
+    listItem.classList.add('active') :
+    listItem.closest('.list-item').classList.add('active');
+});
 
 closeIcon.addEventListener('click', closeDiv);
