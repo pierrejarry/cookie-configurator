6b6a539979793ceca57dec6de4ec69e9177a293a
Pierre Jarry <pierre.jarry82@gmail.com>
2022-05-09 10:19:52 +0200

    Added other options section and tooltip

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 const infoDiv = document.querySelector('.main-content__main__info');
 const closeIcon = document.querySelector('.main-content__main__info .close-icon');
+const iabCheckbox = document.querySelector('.iab');
+const checkboxInfo = document.querySelector('.checkbox-info');
 
 /* Close Information div when clicking on close icon */
 const closeDiv = () => infoDiv.remove();
-
+const toggleCheckboxInfo = () => checkboxInfo.classList.toggle('show');
 
 /* Add active class when clicking on an item list */
-
 document.addEventListener('click', function handleClick(event) {
     const listItem = event.target,
           listParent = listItem.closest('ul'),
@@ -22,3 +23,5 @@ document.addEventListener('click', function handleClick(event) {
 });
 
 closeIcon.addEventListener('click', closeDiv);
+
+iabCheckbox.addEventListener('change', toggleCheckboxInfo);
